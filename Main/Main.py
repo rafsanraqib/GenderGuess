a83a7f039faf376743336fee4fff51975716bb32
Ahmed Rafsan Raqib <46869748+rafsanraqib@users.noreply.github.com>
2019-01-29 14:27:40 -0600

    Update Main.py

--- Main/Main.py
+++ Main/Main.py
@@ -1,11 +1,15 @@
-name = input()
-NameLen = len(name)-1
+while 1 == 1:
+    name = input()
+    NameLength = len(name) -1
 
-if name[NameLen] == 'a':
-    print("Gender: Female/Girl")
+    if name[NameLength] == 'a':
+        print("Gender: Female/Girl")
 
-if name[NameLen-1] == 'a' and name[NameLen] == 'h':
-    print("Gender: Female/Girl")
+    if name[NameLength-1] == 'a' and name[NameLength] == 'h':
+        print("Gender: Female/Girl")
 
-else :
-    print("Gender: Male/Boy")
+    if name[0] == 'q':
+        print("Have a nice day")
+        break
+    else :
+        print ("Gender: Male/Boy")
